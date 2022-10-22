d51a4a23d8dca87cf45e4e15daa6493c7bda1c1d
Momna Ijaz <114725759+momnaijaz10@users.noreply.github.com>
2022-10-22 04:47:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu");
+let menuIcon = document.querySelector(".menu_icon");
+
+menuIcon.addEventListener('click', ()=>{
+  menu.classList.toggle('open');
+})
+  
